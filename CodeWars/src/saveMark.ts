e184c00f3e3d96bde00500731612c66f89d39a39
aRne <arne.schramm@protonmail.com>
2018-04-01 23:06:47 +0200

    Solved saveMark

--- CodeWars/src/saveMark.ts
+++ CodeWars/src/saveMark.ts
@@ -1,5 +1,49 @@
 export function saveMark(c1: string, c2: string): string {
     // Don't be daunted! You can do this. Believe in yourself.
-    return "not solved yet";
+    const separateGeoLocationsC1 : string[] = c1.split(',',2);
+    const separateGeoLocationsC2 : string[] = c2.split(',',2);
+    const radius : number = 3390;
+    const result = haversine(
+        radius,
+        parseFloatFromGeoLocation(separateGeoLocationsC1[0]),
+        parseFloatFromGeoLocation(separateGeoLocationsC1[1]),
+        parseFloatFromGeoLocation(separateGeoLocationsC2[0]),
+        parseFloatFromGeoLocation(separateGeoLocationsC2[1])
+    )
+    return result.toString() + 'KM';
   }
-  
+
+export function haversine(radius: number, latOne: number, longOne: number, latTwo: number, longTwo: number): number{
+    
+    const distanceLongitute = toRadian(longTwo - longOne);
+    const distanceLatitude = toRadian(latTwo - latOne);
+    latOne = toRadian(latOne);
+    latTwo = toRadian(latTwo);
+    const a = 
+        haverPart(distanceLatitude) +
+        haverPart(distanceLongitute) *
+        Math.cos(latOne) *
+        Math.cos(latTwo);
+    const c = Math.asin(Math.sqrt(a));
+    return roundDown(Math.abs((radius * 2 * c)));
+}
+
+function haverPart(input: number):number {
+    return Math.pow(Math.sin(input / 2), 2);
+}
+
+export function toRadian(value: number){
+    const radianFactor = 0.0174533;
+    return value * radianFactor;
+}
+
+export function roundDown(input: number){
+    return input - input % 10;
+}
+
+export function parseFloatFromGeoLocation(location: string){
+    let result = parseFloat(location);
+    return location[location.length-1] === ('W') || location[location.length-1] ===('S') ? 
+        result * -1 :
+        result;    
+}
--- CodeWars/tests/saveMark-tests.ts
+++ CodeWars/tests/saveMark-tests.ts
@@ -1,6 +1,5 @@
 import solution = require('../src/saveMark');
 import { assert } from 'chai';
-import { describe, it } from 'mocha-typescript';
 
 describe('Example Tests', () => {
   it('tests', () => {
@@ -8,4 +7,43 @@ describe('Example Tests', () => {
     assert.equal(solution.saveMark('52.10° S, 56.25° W', '52.10° N, 56.25° W'), '6160KM');
     assert.equal(solution.saveMark('11.28° S, 78.98° E', '21.28° S, 75.56° E'), '620KM');
   });
+
+  it('should calculated haversine correctly', ()=>{
+    //arrange
+    const radius : number = 3390;
+    const pointOne: [number, number] = [48.23, 89.10];
+    const pointTwo: [number, number] = [48.84, 89.40];
+    
+    //act
+    let result = solution.haversine(radius,pointOne[0], pointOne[1], pointTwo[0], pointTwo[1]);
+
+    //assert
+    assert.equal(result, 30);
+  })
+
+  it('should calculate another haversine correctly', ()=>{
+    //arrange
+    const radius : number = 6372.8;
+    
+    //act
+    let result = solution.haversine(radius,36.12, -86.67, 33.94, -118.40);
+
+    //assert
+    assert.equal(result, 2880);
+  })
+
+  it('should round down to multiples of ten', () => {
+      assert.equal(solution.roundDown(31),30);
+      assert.equal(solution.roundDown(36),30);
+      assert.equal(solution.roundDown(131.234),130);
+      assert.equal(solution.roundDown(59.9999),50);
+  })
+
+  it('should parse a float from a geolocation correctly', () => {
+      assert.equal(solution.parseFloatFromGeoLocation('48.23° N'), 48.23);
+      assert.equal(solution.parseFloatFromGeoLocation('48.23° E'), 48.23);
+      assert.equal(solution.parseFloatFromGeoLocation('48.23° S'), -48.23);
+      assert.equal(solution.parseFloatFromGeoLocation('48.23° W'), -48.23);
+  })
+  
 });
